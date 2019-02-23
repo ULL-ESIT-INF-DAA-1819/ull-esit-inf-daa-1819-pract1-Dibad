fd2ac103ef2904f1c22a83c9b7e2348d75575dfd
Dibad <dibadgithub@gmail.com>
2019-02-23 19:52:34 +0000

    Add placeholders for instructions in Alu

--- src/main/java/Ramsim/Alu/Alu.java
+++ src/main/java/Ramsim/Alu/Alu.java
@@ -1,8 +1,11 @@
 package Ramsim.Alu;
 
+import java.util.NoSuchElementException;
+
 import Ramsim.Mem.MemoryManager;
 import Ramsim.Instruction.Opcode;
 import Ramsim.Io.InputUnit;
+import Ramsim.Io.OutputUnit;
 
 public class Alu {
   private int ip_ = 0; // Instruction pointer
@@ -10,6 +13,7 @@ public class Alu {
   // Components
   private MemoryManager memory_;
   private InputUnit input_;
+  private OutputUnit output_;
   private Opcode opcode_;
 
   public Alu(MemoryManager memory) {
@@ -28,6 +32,60 @@ public class Alu {
 
   public void execute() {
     // Get function pointer from opcode ID
+    switch(opcode_.getId()) {
+      case 0:
+        load();
+        break;
+
+      case 1:
+        store();
+        break;
+
+      case 2:
+        add();
+        break;
+
+      case 3:
+        sub();
+        break;
+
+      case 4:
+        // mul();
+        break;
+
+      case 5:
+        // div();
+        break;
+
+      case 6:
+        read();
+        break;
+
+      case 7:
+        write();
+        break;
+
+      case 8:
+        //jump();
+        break;
+
+      case 9:
+        //jzero();
+        break;
+
+      case 10:
+        //jgtz();
+        break;
+
+      case 11:
+        //halt();
+        break;
+
+      default:
+        throw new NoSuchElementException
+          ("Cannot find an instruction associated with opcode" +
+           opcode_.getId() + "!");
+    }
   }
 
   // RAM Simulator Instruction Set
@@ -49,5 +107,9 @@ public class Alu {
 
   private void read() {
     memory_.putInRegister((int)opcode_.getArgument(), input_.read());
+  }
+
+  private void write() {
+    output_.write((int)opcode_.getArgument());
   }
 }
